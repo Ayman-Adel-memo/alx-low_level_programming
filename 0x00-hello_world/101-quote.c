062ac3076c83dd73bbdcfbaba652837b6f3c5b52
Ayman-Adel-memo <adel.ayman227@gmail.com>
2023-06-17 16:31:11 -0700

    sds

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+ * main - Enrty point
+ *
+ * Descraption: print a qsd
+ *	write
+ *	Return: 1(erroe)
+*/
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
